e10a54be849589d4e61dd9f5faf3eceb7958539b
Satoru Tanaka <ittimfn@gmail.com>
2021-04-21 22:15:46 +0900

    サンプル作成

--- app/app.py
+++ app/app.py
@@ -3,3 +3,14 @@ import json
 
 # サンプルのjsonファイルパス
 JSON_FILE_PATH = './input/comments_PbONx9_umgE_20210421_184211.json'
+
+JSON_KEY = ['kind','etag',]
+
+with open(JSON_FILE_PATH, mode='r', ) as f:
+    comments = json.load(f)
+    # マップみたいなもんだと思えばいいっぽい。
+    print(comments['pollingIntervalMillis'])
+
+    itemsIndex = 0
+    print(comments['items'][itemsIndex]['snippet']['displayMessage']); itemsIndex = itemsIndex + 1
+    print(comments['items'][itemsIndex]['snippet']['displayMessage']); itemsIndex = itemsIndex + 1
---
+++ app/model/YoutubeLiveCommentModel.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#
+#json_open = open('qitta_json.json', 'r')
+#
+#{
+#  "kind": "youtube#liveChatMessageListResponse",
+#  "etag": "Wl-653j5XzHALx7ISX3LwCzDHmI",
+#  "pollingIntervalMillis": 4859,
+#  "pageInfo": {
+#    "totalResults": 74,
+#    "resultsPerPage": 74
+#  },
+#  "nextPageToken": "GPbL7IOGj_ACILqk44aGj_AC",
+#  "items": [
+#    {
+#      "kind": "youtube#liveChatMessage",
+#      "etag": "Q-rUqluQtHRcQmTblgwD8aJrn00",
+#      "id": "LCC.CjgKDQoLUGJPTng5X3VtZ0UqJwoYVUNkeXFBYVpES0hYZzRBaGk3VkVOVGhREgtQYk9OeDlfdW1nRRI8ChpDT2pzeHVLRmpfQUNGVE1ONXdvZFd5SUx1ZxIeQ0xHSDg5dUZqX0FDRmM1WFlBb2RSR3dBUlEtMjY1",
+#      "snippet": {
+#        "type": "textMessageEvent",
+#        "liveChatId": "Cg0KC1BiT054OV91bWdFKicKGFVDZHlxQWFaREtIWGc0QWhpN1ZFTlRoURILUGJPTng5X3VtZ0U",
+#        "authorChannelId": "UCyWXFQ9dwfCCg8VNDhrHFAg",
+#        "publishedAt": "2021-04-21T09:41:01.020815+00:00",
+#        "hasDisplayContent": true,
+#        "displayMessage": "いつかね！",
+#        "textMessageDetails": {
+#          "messageText": "いつかね！"
+#        }
+#      },
+#      "authorDetails": {
+#        "channelId": "UCyWXFQ9dwfCCg8VNDhrHFAg",
+#        "channelUrl": "http://www.youtube.com/channel/UCyWXFQ9dwfCCg8VNDhrHFAg",
+#        "displayName": "朱",
+#        "profileImageUrl": "https://yt3.ggpht.com/ytc/AAUvwnjjWV7j_tV8u9f1vkyKVT_HFDYZCb2Yk0PfHZO-=s88-c-k-c0x00ffffff-no-rj",
+#        "isVerified": false,
+#        "isChatOwner": false,
+#        "isChatSponsor": true,
+#        "isChatModerator": false
+#      }
+#    },
